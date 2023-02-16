6a0ec12a1b8b137ba2681fd79dd663b67ac68071
jesusp254455 <jpenaranda633@gmail.com>
2023-02-16 12:01:12 -0500

    jp

--- src/components/Listuser.jsx
+++ src/components/Listuser.jsx
@@ -10,7 +10,6 @@ const Listuser = () => {
   const [id, setid] = useState('')
   const [name, setname] = useState('')
   const [email, setemail] = useState('')
-  const [password, setpassword] = useState('')
   const [consulta, setconsulta] = useState('')
   const [title, settitle] = useState('')
 
@@ -21,19 +20,35 @@ const Listuser = () => {
   const getusers = async () => {
     const respuesta = await axios.get(url);
     setusers(respuesta.data);
-    console.log(users);
   }
+
+  const modal = (op, id, name, email) => {
+    setname('')
+    setid('')
+    setemail('')
+    setconsulta(op)
+    if (op === 1) {
+      settitle('Registrar usuario');
+    } else if(op === 2) {
+      settitle('actualizar informacion')
+      setname(name)
+      setid(id)
+      setemail(email)
+    }
+    
+  }
+  
   return (
     <>
       <div class="d-grid gap-2 col-6 mx-auto mt-4">
-        <button class="btn btn-outline-warning" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Registrar</button>
+        <button class="btn btn-outline-warning" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>modal(1)}>Registrar</button>
       </div>
 
       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Registrar usuario</h1>
+              <h1 class="modal-title fs-5" id="exampleModalLabel">{title}</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -42,19 +57,13 @@ const Listuser = () => {
                 <div class="mb-3 row mt-3">
                   <label for="inputNombre" class="col-sm-2 col-form-label">Nombre</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" id="Nombre" />
+                    <input onChange={(e) => setname(e.target.value)} type="text" class="form-control" id="Nombre" value={name} />
                   </div>
                 </div>
                 <div class="mb-3 row mt-3">
                   <label for="email" class="col-sm-2 col-form-label">Email</label>
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" />
-                  </div>
-                </div>
-                <div class="mb-3 row mt-3">
-                  <label for="password" class="col-sm-2 col-form-label">Password</label>
-                  <div class="col-sm-10">
-                    <input type="password" class="form-control" id="password" />
+                    <input onChange={(e) => setemail(e.target.value)} type="email" class="form-control" id="email" value={email} />
                   </div>
                 </div>
                 <div class="modal-footer">
@@ -73,23 +82,21 @@ const Listuser = () => {
             <tr className="text-center">
               <th scope="col">Nombre</th>
               <th scope="col">Email</th>
-              <th scope="col">Contraseña</th>
               <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
-          {
-            users.map( (u,i)=>(
-              <tr key={u.id} className="text-center">
-                  <td>{(i+1)}</td>
+            {
+              users.map((u, i) => (
+                <tr key={u.id} className="text-center">
+                  
                   <td>{u.name}</td>
                   <td>{u.email}</td>
-                  <td>{u.password}</td>
-                  <td><button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#Modal">Editar</button>&nbsp;&nbsp;
+                  <td><button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>modal(2,u.id,u.name,u.email)}>Editar</button>&nbsp;&nbsp;
                     <button type="button" class="btn btn-outline-danger" >Eliminar</button> </td>
                 </tr>
-            ))
-          }
+              ))
+            }
           </tbody>
         </table>
       </div>
